efa5ee5bc9e2630c31aa660d4782975d756ec347
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2024-10-09 13:05:04 -0700

    Update closures1.js

--- javascript/closures1.js
+++ javascript/closures1.js
@@ -12,3 +12,17 @@ const closureExample = outerFunction();
 closureExample();
 
 
+//////////////////////////////////////////////////////
+
+function fun1() {
+    let a = 5;
+
+    function fun2() {
+        console.log(a)
+    }
+    a=8;
+    return fun2;
+}
+
+const abc = fun1();
+abc();             // we do not retirn variable value we return variable reference
